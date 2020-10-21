91239d1088ec7ac34538e40d3589943e2428097f
evanshango <evansonshango@gmail.com>
2020-10-21 21:28:26 +0300

    handle fetching songs from firebase

--- app/src/main/java/com/evans/playnow/helpers/Constants.kt
+++ app/src/main/java/com/evans/playnow/helpers/Constants.kt
@@ -3,4 +3,5 @@ package com.evans.playnow.helpers
 object Constants {
 
     const val SONG_COLLECTION = "songs"
+    const val SERVICE_TAG = "MusicService"
 }
---
+++ app/src/main/java/com/evans/playnow/helpers/State.kt
@@ -0,0 +1,8 @@
+package com.evans.playnow.helpers
+
+enum class State {
+    STATE_CREATED,
+    STATE_INITIALIZING,
+    STATE_INITIALIZED,
+    STATE_ERROR
+}
