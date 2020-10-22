e349524c60c17da053e04147a2e000a4fcbe59a6
evanshango <evansonshango@gmail.com>
2020-10-22 16:16:21 +0300

    toggle the play state of a song in the song viewpager

--- app/src/main/java/com/evans/playnow/ui/MainActivity.kt
+++ app/src/main/java/com/evans/playnow/ui/MainActivity.kt
@@ -1,15 +1,20 @@
 package com.evans.playnow.ui
 
 import android.os.Bundle
+import android.support.v4.media.session.PlaybackStateCompat
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager2.widget.ViewPager2
 import com.bumptech.glide.RequestManager
+import com.evans.playnow.R
 import com.evans.playnow.adapters.SwipeSongAdapter
 import com.evans.playnow.data.entities.Song
 import com.evans.playnow.databinding.ActivityMainBinding
+import com.evans.playnow.exoplayer.isPlaying
 import com.evans.playnow.exoplayer.toSong
 import com.evans.playnow.helpers.Status.*
 import com.evans.playnow.ui.viewmodels.MainViewModel
+import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
 import javax.inject.Inject
 
@@ -27,6 +32,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var glide: RequestManager
 
     private var currPlayingSong: Song? = null
+    private var playbackState: PlaybackStateCompat? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,6 +42,23 @@ class MainActivity : AppCompatActivity() {
 
         binding.apply {
             songViewpager.adapter = swipeSongAdapter
+
+            songViewpager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
+                override fun onPageSelected(position: Int) {
+                    super.onPageSelected(position)
+                    if (playbackState?.isPlaying == true) {
+                        mainViewModel.playOrToggleSong(swipeSongAdapter.songs[position])
+                    } else {
+                        currPlayingSong = swipeSongAdapter.songs[position]
+                    }
+                }
+            })
+
+            ivPlayPause.setOnClickListener {
+                currPlayingSong?.let {
+                    mainViewModel.playOrToggleSong(it, true)
+                }
+            }
         }
     }
 
@@ -72,6 +95,32 @@ class MainActivity : AppCompatActivity() {
                 currPlayingSong = it.toSong()
                 glide.load(currPlayingSong?.imgUrl).into(ivCurrSong)
                 switchViewPagerToCurrSong(currPlayingSong ?: return@observe)
+            }
+            mainViewModel.playbackState.observe(this@MainActivity) {
+                playbackState = it
+                ivPlayPause.setImageResource(
+                    if (playbackState?.isPlaying == true) R.drawable.ic_pause else R.drawable.ic_play
+                )
+            }
+            mainViewModel.isConnected.observe(this@MainActivity) {
+                it?.getContentIfNotHandled()?.let { result ->
+                    when (result.status) {
+                        ERROR -> Snackbar.make(
+                            root, result.message ?: "Something went wrong", Snackbar.LENGTH_LONG
+                        ).show()
+                        else -> Unit
+                    }
+                }
+            }
+            mainViewModel.networkError.observe(this@MainActivity) {
+                it?.getContentIfNotHandled()?.let { result ->
+                    when (result.status) {
+                        ERROR -> Snackbar.make(
+                            root, result.message ?: "Something went wrong", Snackbar.LENGTH_LONG
+                        ).show()
+                        else -> Unit
+                    }
+                }
             }
         }
     }
